0930a85182a85108d9f9c6505d1375ca7ab5f1b5
abdulazizahwan <abdulazizahwan97@gmail.com>
2019-07-26 01:50:54 +0700

    initial commit

---
+++ Stanford University Student Login/Form1.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.txtIDNumber = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnRegister = New System.Windows.Forms.Button()
+        Me.BunifuElipse1 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.BunifuElipse2 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.BunifuElipse3 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.BunifuElipse4 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.BunifuElipse5 = New Bunifu.Framework.UI.BunifuElipse(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(140, Byte), Integer), CType(CType(21, Byte), Integer), CType(CType(21, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.btnLogin)
+        Me.Panel1.Controls.Add(Me.Panel3)
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel1.Location = New System.Drawing.Point(480, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(292, 515)
+        Me.Panel1.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Source Sans Pro Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(38, 168)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(190, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Or login with your account"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Source Sans Pro", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(154, 293)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(98, 15)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Forgot password?"
+        '
+        'btnLogin
+        '
+        Me.btnLogin.BackColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.btnLogin.FlatAppearance.BorderSize = 0
+        Me.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnLogin.Font = New System.Drawing.Font("Source Sans Pro Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLogin.ForeColor = System.Drawing.Color.FromArgb(CType(CType(140, Byte), Integer), CType(CType(21, Byte), Integer), CType(CType(21, Byte), Integer))
+        Me.btnLogin.Location = New System.Drawing.Point(42, 285)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(75, 29)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "LOGIN"
+        Me.btnLogin.UseVisualStyleBackColor = False
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.Panel3.Controls.Add(Me.txtPassword)
+        Me.Panel3.Location = New System.Drawing.Point(42, 236)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(210, 29)
+        Me.Panel3.TabIndex = 3
+        '
+        'txtPassword
+        '
+        Me.txtPassword.BackColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtPassword.Font = New System.Drawing.Font("Source Sans Pro Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPassword.Location = New System.Drawing.Point(9, 4)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(191, 21)
+        Me.txtPassword.TabIndex = 2
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.Panel2.Controls.Add(Me.txtIDNumber)
+        Me.Panel2.Location = New System.Drawing.Point(42, 199)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(210, 29)
+        Me.Panel2.TabIndex = 2
+        '
+        'txtIDNumber
+        '
+        Me.txtIDNumber.BackColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.txtIDNumber.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtIDNumber.Font = New System.Drawing.Font("Source Sans Pro Light", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtIDNumber.Location = New System.Drawing.Point(9, 4)
+        Me.txtIDNumber.Name = "txtIDNumber"
+        Me.txtIDNumber.Size = New System.Drawing.Size(191, 21)
+        Me.txtIDNumber.TabIndex = 2
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.Stanford_University_Student_Login.My.Resources.Resources.stanford_logo__1_
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(17, 51)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(175, 68)
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Source Sans Pro Semibold", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.Label3.Location = New System.Drawing.Point(35, 134)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(121, 34)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Welcome"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Source Sans Pro", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(118, Byte), Integer), CType(CType(132, Byte), Integer), CType(CType(158, Byte), Integer))
+        Me.Label4.Location = New System.Drawing.Point(37, 168)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(211, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Register for a Student account"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Source Sans Pro", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(118, Byte), Integer), CType(CType(132, Byte), Integer), CType(CType(158, Byte), Integer))
+        Me.Label5.Location = New System.Drawing.Point(37, 197)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 20)
+        Me.Label5.TabIndex = 8
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(54, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Font = New System.Drawing.Font("Source Sans Pro", 8.999999!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(118, Byte), Integer), CType(CType(132, Byte), Integer), CType(CType(158, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(41, 197)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(412, 128)
+        Me.TextBox1.TabIndex = 10
+        Me.TextBox1.Text = resources.GetString("TextBox1.Text")
+        '
+        'btnRegister
+        '
+        Me.btnRegister.BackColor = System.Drawing.Color.FromArgb(CType(CType(140, Byte), Integer), CType(CType(21, Byte), Integer), CType(CType(21, Byte), Integer))
+        Me.btnRegister.FlatAppearance.BorderSize = 0
+        Me.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnRegister.Font = New System.Drawing.Font("Source Sans Pro Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRegister.ForeColor = System.Drawing.Color.FromArgb(CType(CType(54, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.btnRegister.Location = New System.Drawing.Point(41, 331)
+        Me.btnRegister.Name = "btnRegister"
+        Me.btnRegister.Size = New System.Drawing.Size(129, 29)
+        Me.btnRegister.TabIndex = 7
+        Me.btnRegister.Text = "REGISTER"
+        Me.btnRegister.UseVisualStyleBackColor = False
+        '
+        'BunifuElipse1
+        '
+        Me.BunifuElipse1.ElipseRadius = 5
+        Me.BunifuElipse1.TargetControl = Me
+        '
+        'BunifuElipse2
+        '
+        Me.BunifuElipse2.ElipseRadius = 5
+        Me.BunifuElipse2.TargetControl = Me.Panel2
+        '
+        'BunifuElipse3
+        '
+        Me.BunifuElipse3.ElipseRadius = 5
+        Me.BunifuElipse3.TargetControl = Me.Panel3
+        '
+        'BunifuElipse4
+        '
+        Me.BunifuElipse4.ElipseRadius = 5
+        Me.BunifuElipse4.TargetControl = Me.btnLogin
+        '
+        'BunifuElipse5
+        '
+        Me.BunifuElipse5.ElipseRadius = 5
+        Me.BunifuElipse5.TargetControl = Me.btnRegister
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(54, Byte), Integer), CType(CType(60, Byte), Integer), CType(CType(72, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(772, 515)
+        Me.Controls.Add(Me.btnRegister)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents txtIDNumber As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btnRegister As Button
+    Friend WithEvents BunifuElipse1 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents BunifuElipse2 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents BunifuElipse3 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents BunifuElipse4 As Bunifu.Framework.UI.BunifuElipse
+    Friend WithEvents BunifuElipse5 As Bunifu.Framework.UI.BunifuElipse
+End Class
---
+++ Stanford University Student Login/Form1.vb
@@ -0,0 +1,45 @@
+﻿Public Class Form1
+    Private Sub txtIDNumber_MouseEnter(sender As Object, e As EventArgs) Handles txtIDNumber.MouseEnter
+        Dim colorText As Color
+        colorText = Color.FromArgb(54, 60, 72)
+        If txtIDNumber.Text = "Student ID Number" Then
+            txtIDNumber.Text = ""
+            txtIDNumber.ForeColor = colorText
+        End If
+    End Sub
+
+    Private Sub txtIDNumber_MouseLeave(sender As Object, e As EventArgs) Handles txtIDNumber.MouseLeave
+        Dim colorHint As Color
+        colorHint = Color.FromArgb(118, 132, 158)
+        If txtIDNumber.Text = "" Then
+            txtIDNumber.Text = "Student ID Number"
+            txtIDNumber.ForeColor = colorHint
+        End If
+    End Sub
+
+    Private Sub txtPassword_MouseEnter(sender As Object, e As EventArgs) Handles txtPassword.MouseEnter
+        Dim colorText As Color
+        colorText = Color.FromArgb(54, 60, 72)
+        If txtPassword.Text = "Password" Then
+            txtPassword.Text = ""
+            txtPassword.PasswordChar = "*"
+            txtPassword.ForeColor = colorText
+        End If
+    End Sub
+
+    Private Sub txtPassword_MouseLeave(sender As Object, e As EventArgs) Handles txtPassword.MouseLeave
+        Dim colorHint As Color
+        colorHint = Color.FromArgb(118, 132, 158)
+        If txtPassword.Text = "" Then
+            txtPassword.Text = "Password"
+            txtPassword.PasswordChar = ""
+            txtPassword.ForeColor = colorHint
+        End If
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Me.Hide()
+        Dim Form2 As New Form2
+        Form2.Show()
+    End Sub
+End Class
